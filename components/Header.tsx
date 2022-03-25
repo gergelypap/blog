2e158248e01b74d8604e443ce49a71cc2b4069fa
Gergely Pap <pgege42@gmail.com>
2022-03-25 10:13:55 +0100

    Some basic changes

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>@gpap</h1>
+      <p>My developer blog. Because everybody has one.</p>
+      <Link href="/">
+        <a>Posts</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </header>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "@components/Header";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
