436ba2cb1c003d8d2ca18756fad6c6a276e5e909
Gergely Pap <pgege42@gmail.com>
2022-04-13 16:23:36 +0200

    fix: fix regex bug in filename validation

--- lib/posts.ts
+++ lib/posts.ts
@@ -30,7 +30,7 @@ export async function getPosts() {
 export function getPostSlugs(): string[] {
   return postFiles.map((path) => {
     // Ensure files are in correct format.
-    if (!/^\d{4}-\d{2}-\d{2}-[a-z0-9]+$/.test(path)) {
+    if (!/^\d{4}-\d{2}-\d{2}-[a-z0-9-]+$/.test(path)) {
       throw new Error(`Post file '${path}' has wrong filename format.`);
     }
 
