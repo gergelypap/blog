6d08794c84ebfc932b461248375e6e7d2854ceec
Gergely Pap <pgege42@gmail.com>
2022-04-16 18:56:49 +0200

    feat(spotify): remove cache-control header

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -14,8 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const artist = song.item.artists.map((a: { name: string }) => a.name).join(", ");
   const url = song.item.external_urls.spotify;
 
-  res.setHeader("Cache-Control", "public, s-maxage=60, stale-while-revalidate=30");
-
   return res.status(200).json({
     isPlaying,
     title,
