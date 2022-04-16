0fae6305edb5b0674aa6122ef836c7ba13d11536
Gergely Pap <pgege42@gmail.com>
2022-04-17 00:00:07 +0200

    feat(blog): add basic filter for posts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,19 +4,33 @@ import { PostType } from "@type/Post";
 import { APP_NAME } from "@utils/constants";
 import type { GetStaticProps } from "next";
 import Head from "next/head";
+import { useState } from "react";
 
 interface Props {
   posts: PostType[];
 }
 
 export default function BlogPostsPage({ posts }: Props) {
+  const [searchValue, setSearchValue] = useState("");
+  const filteredPosts = posts.filter((post) =>
+    `${post.meta.title} ${post.meta.lead}`.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <>
       <Head>
         <title>Blog / {APP_NAME}</title>
       </Head>
       <section>
-        {posts.map((post, i) => (
+        <input
+          className="block w-full px-4 py-2 text-gray-900 border  border-gray-200 rounded-md dark:border-gray-900 focus:ring-gray-900 focus:border-gray-900 dark:bg-gray-700 dark:text-gray-100 mb-10"
+          aria-label="Search posts"
+          placeholder="Search posts..."
+          type="search"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        {!filteredPosts.length && <p>No posts found.</p>}
+        {filteredPosts.map((post, i) => (
           <Post key={i} post={post} />
         ))}
       </section>
