14d36b90c3e51300377463e305e684ad5d72ef37
Gergely Pap <pgege42@gmail.com>
2022-04-19 17:33:06 +0200

    chore: add any to fetcher json type

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,4 +1,5 @@
-export default async function fetcher<JSON>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
   const res = await fetch(input, init);
 
   return res.json();
