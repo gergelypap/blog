f4aa1f268445cf28b6b956276a177b812af7195a
Gergely Pap <pgege42@gmail.com>
2022-04-21 20:19:25 +0200

    chore: change 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import DefaultLayout from "@components/Layout/DefaultLayout";
 
 export default function Custom404() {
   return (
-    <div className="text-center">
-      <h1 className="text-center text-3xl mb-[-2rem]">Page Not Found!</h1>
-      <Image className="scale-150 ]" src="/img/404.svg" width="550" height="550" alt="404" />
-    </div>
+    <DefaultLayout>
+      <h1 className="text-center text-5xl">404</h1>
+      <h1 className="text-center text-5xl">Page Not Found</h1>
+    </DefaultLayout>
   );
 }
