4559d51c247f60bf8ddaedfa4d867d631304217b
Gergely Pap <pgege42@gmail.com>
2022-04-21 09:15:30 +0200

    chore: remove pageExtensions from next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
 module.exports = nextConfig;
