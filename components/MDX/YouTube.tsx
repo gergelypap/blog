02c93224ca4711c022cb9b19498db7d480dc047a
Gergely Pap <pgege42@gmail.com>
2022-04-22 08:53:05 +0200

    feat(blog): move youtube component and add styles to it

--- components/MDX/YouTube.tsx
+++ components/MDX/YouTube.tsx
@@ -1,18 +1,17 @@
 interface Props {
   videoId: string;
+  className?: string;
 }
 
-export default function YouTube({ videoId }: Props) {
+export default function YouTube({ videoId, className = undefined }: Props) {
   return (
     <iframe
-      width="640"
-      height="320"
       src={`https://www.youtube.com/embed/${videoId}?showinfo=0&rel=0&iv_load_policy=3&color=white&modestbranding=1`}
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-      className="w-full mb-10"
+      className={["aspect-video w-full mb-5", className].join(" ")}
     ></iframe>
   );
 }
