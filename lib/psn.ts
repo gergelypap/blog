370b432065414043f6acbd678500cfff851fd997
Gergely Pap <pgege42@gmail.com>
2022-04-23 18:53:05 +0200

    chore: remove comments

--- lib/psn.ts
+++ lib/psn.ts
@@ -26,18 +26,12 @@ async function authorize() {
 
 export default async function getLastPlayedGame() {
   const auth = await authorize();
-
-  // Get the user's games.
   const userTitles = await getUserTitles({ accessToken: auth.accessToken }, "me");
-
-  // Find the first game which has any earned trophies.
   const lastPlayedTitle = userTitles.trophyTitles.find((title) => Object.values(title.earnedTrophies).some(Boolean));
-
   if (!lastPlayedTitle) {
     throw new Error("Could not fetch last played title.");
   }
 
-  // Get the user's trophies for the latest played game.
   const { trophies: userTrophies } = await getUserTrophiesEarnedForTitle(
     { accessToken: auth.accessToken },
     "me",
@@ -46,12 +40,9 @@ export default async function getLastPlayedGame() {
     { npServiceName: "trophy" }
   );
 
-  // Find the last earned trophy from the user's game trophies.
   const lastEarnedTrophy = userTrophies
     .filter((trophy) => trophy.earned)
     .sort((a, b) => +new Date(a.earnedDateTime as string) - +new Date(b.earnedDateTime as string))[0];
-
-  // Get the trophy information for the game, because it contains details about the trophy.
   const { trophies } = await getTitleTrophies(
     { accessToken: auth.accessToken },
     lastPlayedTitle.npCommunicationId,
@@ -59,9 +50,7 @@ export default async function getLastPlayedGame() {
     { npServiceName: "trophy" }
   );
 
-  // Finally get the trophy's details, like name and icon.
   const trophy = trophies.find((trophy) => trophy.trophyId === lastEarnedTrophy.trophyId);
-
   if (!trophy) {
     throw new Error("Could not fetch trophy details.");
   }
