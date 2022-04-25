4199f5914c413e9234bbf02f7100975a2a66dcbf
Gergely Pap <pgege42@gmail.com>
2022-04-25 20:24:51 +0200

    feat(games): add game listing page

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,10 +1,39 @@
-import TicTacToe from "@components/Games/TicTacToe";
 import DefaultLayout from "@components/Layout/DefaultLayout";
+import Link from "@components/Link";
+import PageTitle from "@components/PageTitle";
+import { readdirSync } from "fs";
+import type { GetStaticProps } from "next";
+import { join } from "path";
 
-export default function GamesPage() {
+interface Props {
+  games: string[];
+}
+
+export default function GamesPage({ games }: Props) {
   return (
     <DefaultLayout>
-      <TicTacToe />
+      <PageTitle>Games</PageTitle>
+      <p>Little, simple games I made in React. Have fun!</p>
+      <section className="sm:grid sm:grid-cols-2 sm:gap-10">
+        <ul>
+          {games.map((game, i) => (
+            <li key={i}>
+              <Link href={`/games/${game}`}>{game}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </DefaultLayout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const gameComponents = readdirSync(join(process.cwd(), "components", "Games"));
+  const games = gameComponents.map((game) => game.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase());
+
+  return {
+    props: {
+      games,
+    },
+  };
+};
---
+++ pages/games/tic-tac-toe.tsx
@@ -0,0 +1,10 @@
+import TicTacToe from "@components/Games/TicTacToe";
+import DefaultLayout from "@components/Layout/DefaultLayout";
+
+export default function TicTacToeGame() {
+  return (
+    <DefaultLayout>
+      <TicTacToe />
+    </DefaultLayout>
+  );
+}
