7bb06c02e47e2b6333c557bda1abadce70e53bc3
Gergely Pap <pgege42@gmail.com>
2022-05-05 17:29:39 +0200

    chore: move favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,20 +17,20 @@ class MyDocument extends Document {
           <link rel="preload" href="/api/psn" as="fetch" crossOrigin="anonymous" />
 
           {/* Favicons and theming */}
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
-          <link rel="manifest" href="/manifest.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <link rel="yandex-tableau-widget" href="/yandex-browser-manifest.json" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="48x48" href="/favicons/favicon-48x48.png" />
+          <link rel="manifest" href="/favicons/manifest.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="yandex-tableau-widget" href="/favicons/yandex-browser-manifest.json" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="@gpap" />
           <meta name="msapplication-TileColor" content="#111827" />
-          <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png" />
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#111827" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f9fafb" />
           <meta name="mobile-web-app-capable" content="yes" />
