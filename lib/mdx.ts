df26971a5c0d906ae46d9f6c82c551e39262d404
Gergely Pap <pgege42@gmail.com>
2022-05-05 17:27:17 +0200

    chore: remove remark images

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,12 +1,10 @@
 import type { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import rehypeCodeTitles from "rehype-code-titles";
-import remarkImages from "remark-images";
 
 export async function parseMDX(fileContents: string): Promise<MDXRemoteSerializeResult> {
   return await serialize(fileContents, {
     mdxOptions: {
-      remarkPlugins: [remarkImages],
       rehypePlugins: [rehypeCodeTitles],
     },
     parseFrontmatter: true,
