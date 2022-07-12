a1214ae8e40311a1db800be2c478afc055aebca1
Gergely Pap <pgege42@gmail.com>
2022-07-12 13:34:18 +0200

    refactor: clean up mdx config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,14 +1,7 @@
 import type { ComputedFieldResolver } from "contentlayer/core";
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import { readFileSync } from "fs";
-import { join } from "path";
 import readingTime from "reading-time";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode, { type Options } from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-import { remarkMdxImages } from "remark-mdx-images";
-import remarkUnwrapImages from "remark-unwrap-images";
+import mdxOptions from "./lib/mdx";
 
 const dateResolver: ComputedFieldResolver = (doc) => {
   const match = doc._raw.sourceFileDir.match(/\d{4}-\d{2}-\d{2}/);
@@ -112,52 +105,8 @@ export const Snippet = defineDocumentType(() => ({
   },
 }));
 
-const rehypePrettyCodeOptions: Partial<Options> = {
-  theme: {
-    dark: JSON.parse(readFileSync("node_modules/shiki/themes/material-palenight.json").toString()),
-    light: JSON.parse(readFileSync("node_modules/shiki/themes/github-light.json").toString()),
-  },
-  onVisitHighlightedLine(node) {
-    node.properties.className.push("highlighted");
-  },
-  onVisitHighlightedWord(node) {
-    node.properties.className = ["word"];
-  },
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: "text", value: " " }];
-    }
-  },
-};
-
 export default makeSource({
   contentDirPath: "content",
   documentTypes: [Post, Snippet],
-  mdx: {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    remarkPlugins: [remarkUnwrapImages, remarkMdxImages, remarkGfm],
-    rehypePlugins: [
-      [rehypePrettyCode, rehypePrettyCodeOptions],
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "wrap" }],
-    ],
-    esbuildOptions: (options) => {
-      options.loader = {
-        ...options.loader,
-        ".png": "file",
-        ".jpg": "file",
-        ".jpeg": "file",
-        ".webp": "file",
-        ".gif": "file",
-      };
-      options.outdir = join(process.cwd(), "public/img/generated");
-      options.publicPath = "/img/generated";
-      options.write = true;
-
-      return options;
-    },
-  },
+  mdx: mdxOptions,
 });
---
+++ lib/mdx.ts
@@ -0,0 +1,57 @@
+import type { MDXOptions } from "contentlayer/core";
+import { readFileSync } from "fs";
+import type { ESBuildOptions } from "mdx-bundler/dist/types";
+import { join } from "path";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode, { type Options as RehypePrettyCodeOptions } from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import { remarkMdxImages } from "remark-mdx-images";
+import remarkUnwrapImages from "remark-unwrap-images";
+
+const rehypePrettyCodeOptions: Partial<RehypePrettyCodeOptions> = {
+  theme: {
+    // TODO: Why require or import does not work?
+    dark: JSON.parse(readFileSync("node_modules/shiki/themes/material-palenight.json").toString()),
+    light: JSON.parse(readFileSync("node_modules/shiki/themes/github-light.json").toString()),
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }];
+    }
+  },
+};
+
+const mdxOptions: MDXOptions = {
+  remarkPlugins: [remarkUnwrapImages, remarkMdxImages, remarkGfm],
+  rehypePlugins: [
+    [rehypePrettyCode, rehypePrettyCodeOptions],
+    rehypeSlug,
+    [rehypeAutolinkHeadings, { behavior: "wrap" }],
+  ],
+  esbuildOptions: (options: ESBuildOptions) => {
+    options.loader = {
+      ...options.loader,
+      ".png": "file",
+      ".jpg": "file",
+      ".jpeg": "file",
+      ".webp": "file",
+      ".gif": "file",
+    };
+    options.outdir = join(process.cwd(), "public/img/generated");
+    options.publicPath = "/img/generated";
+    options.write = true;
+
+    return options;
+  },
+};
+
+export default mdxOptions;
